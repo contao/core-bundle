a9ee12ea2062f6b64725a4a9e0b04921e8411ab7
Leo Feyer <github@contao.org>
2014-09-05 17:25:18 +0200

    Port the changes from contao/core@b6f5d0861be639654c5b019fcf7e035903e77a27

--- contao/library/Widget.php
+++ contao/library/Widget.php
@@ -278,14 +278,7 @@ abstract class Widget extends View
 
 			case 'disabled':
 			case 'readonly':
-				if ($varValue)
-				{
-					$this->blnSubmitInput = false;
-				}
-				else
-				{
-					$this->blnSubmitInput = true;
-				}
+				$this->blnSubmitInput = $varValue ? false : true;
 				// Do not add a break; statement here
 
 			case 'autofocus':
@@ -325,6 +318,12 @@ abstract class Widget extends View
 
 			case 'dataContainer':
 				$this->objDca = $varValue;
+				break;
+
+			case strncmp($strKey, 'ng-', 3) === 0:
+			case strncmp($strKey, 'data-', 5) === 0:
+				dump($strKey);
+				$this->arrAttributes[$strKey] = $strKey;
 				break;
 
 			default:
