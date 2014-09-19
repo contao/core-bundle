e34870f567d0857cbe9224b839311d2770c33d22
Leo Feyer <github@contao.org>
2014-09-19 16:49:50 +0200

    Port the changes from contao/core@5ef368987c7d560ddafd6c94793f81f765bd862d

--- contao/library/Widget.php
+++ contao/library/Widget.php
@@ -349,15 +349,18 @@ abstract class Widget extends View
 	 *
 	 * Supported keys:
 	 *
-	 * * id:       the field ID
-	 * * name:     the field name
-	 * * label:    the field label
-	 * * value:    the field value
-	 * * class:    one or more CSS classes
-	 * * prefix:   the CSS class prefix
-	 * * template: the template name
-	 * * wizard:   the field wizard markup
-	 * * required: makes the widget a required field
+	 * * id:            the field ID
+	 * * name:          the field name
+	 * * label:         the field label
+	 * * value:         the field value
+	 * * class:         one or more CSS classes
+	 * * prefix:        the CSS class prefix
+	 * * template:      the template name
+	 * * wizard:        the field wizard markup
+	 * * required:      makes the widget a required field
+	 * * forAttribute:  the "for" attribute
+	 * * dataContainer: the data container object
+	 * * activeRecord:  the active record
 	 *
 	 * @param string $strKey The property name
 	 *
@@ -805,6 +808,16 @@ abstract class Widget extends View
 			$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['maxlength'], $this->strLabel, $this->maxlength));
 		}
 
+		if ($this->minval && is_numeric($varInput) && $varInput < $this->minval)
+		{
+			$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['minval'], $this->strLabel, $this->minval));
+		}
+
+		if ($this->maxval && is_numeric($varInput) && $varInput > $this->maxval)
+		{
+			$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['maxval'], $this->strLabel, $this->maxval));
+		}
+
 		if ($this->rgxp != '')
 		{
 			switch ($this->rgxp)
@@ -830,6 +843,14 @@ abstract class Widget extends View
 					if (!Validator::isNumeric($varInput))
 					{
 						$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['digit'], $this->strLabel));
+					}
+					break;
+
+				// Natural numbers (positive integers)
+				case 'natural':
+					if (!Validator::isNatural($varInput))
+					{
+						$this->addError(sprintf($GLOBALS['TL_LANG']['ERR']['natural'], $this->strLabel));
 					}
 					break;
 
