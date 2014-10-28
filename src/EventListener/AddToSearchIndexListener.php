00f15956e0b89797bed554c0cad16fb9e81c5530
Leo Feyer <github@contao.org>
2014-10-28 23:35:59 +0100

    Move the "add to search index logic" back into the legacy code

--- src/EventListener/AddToSearchIndexListener.php
+++ src/EventListener/AddToSearchIndexListener.php
@@ -11,9 +11,7 @@
 
 namespace Contao\Bundle\CoreBundle\EventListener;
 
-use Contao\Config;
-use Contao\Environment;
-use Contao\Search;
+use Contao\Frontend;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 
 /**
@@ -23,21 +21,6 @@ use Symfony\Component\HttpKernel\Event\PostResponseEvent;
  */
 class AddToSearchIndexListener
 {
-    /**
-     * @var Config
-     */
-    protected $config;
-
-    /**
-     * Constructor
-     *
-     * @param Config $config The Contao configuration object
-     */
-    public function __construct(Config $config)
-    {
-        $this->config = $config;
-    }
-
     /**
      * Adds a page to the search index
      *
@@ -51,35 +34,6 @@ class AddToSearchIndexListener
             return;
         }
 
-        // Index page if searching is allowed and there is no back end user
-        if ($this->config->get('enableSearch') && 'regular' === $objPage->type && !BE_USER_LOGGED_IN && !$objPage->noSearch) {
-
-            // Index protected pages if enabled
-            if ($this->config->get('indexProtected') || (!FE_USER_LOGGED_IN && !$objPage->protected)) {
-                $blnIndex = true;
-
-                // Do not index the page if certain parameters are set
-                foreach (array_keys($_GET) as $key) {
-                    if (in_array($key, $GLOBALS['TL_NOINDEX_KEYS']) || 0 === strncmp($key, 'page_', 5)) {
-                        $blnIndex = false;
-                        break;
-                    }
-                }
-
-                if ($blnIndex) {
-                    $arrData = [
-                        'url'       => Environment::get('request'),
-                        'content'   => $event->getResponse()->getContent(),
-                        'title'     => $objPage->pageTitle ?: $objPage->title,
-                        'protected' => ($objPage->protected ? '1' : ''),
-                        'groups'    => $objPage->groups,
-                        'pid'       => $objPage->id,
-                        'language'  => $objPage->language
-                    ];
-
-                    Search::indexPage($arrData);
-                }
-            }
-        }
+        Frontend::indexPageIfApplicable($objPage, $event->getResponse());
     }
 }
