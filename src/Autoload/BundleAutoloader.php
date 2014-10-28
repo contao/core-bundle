3c56713fc49653fb8163044326d474d4136cc11e
Leo Feyer <github@contao.org>
2014-10-28 19:56:34 +0100

    Move the bundle sorting logic from the BundleAutoloader to the ConfigCollection

--- src/Autoload/BundleAutoloader.php
+++ src/Autoload/BundleAutoloader.php
@@ -11,12 +11,11 @@
 
 namespace Contao\Bundle\CoreBundle\Autoload;
 
-use Contao\Bundle\CoreBundle\Exception\UnresolvableLoadingOrderException;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 
 /**
- * Finds the autoload bundles and orders them
+ * Finds the autoload bundles
  *
  * @author Leo Feyer <https://contao.org>
  */
@@ -45,69 +44,18 @@ class BundleAutoloader
     }
 
     /**
-     * Returns the root directory
+     * Returns an ordered bundle collection
      *
-     * @return string The root directory
-     */
-    public function getRootDir()
-    {
-        return $this->rootDir;
-    }
-
-    /**
-     * Returns the environment
-     *
-     * @return string The environment
-     */
-    public function getEnvironment()
-    {
-        return $this->environment;
-    }
-
-    /**
-     * Returns an ordered array of bundles to register
-     *
-     * @return array The ordered bundles
+     * @return ConfigCollection|ConfigCollection[] The ordered bundles
      */
     public function load()
     {
-        $bundles      = [];
-        $collection   = $this->createCollection();
-        $replaces     = $this->getReplacesFromCollection($collection);
-        $loadingOrder = $this->getLoadingOrderFromCollection($collection);
-
-        foreach ($collection as $config) {
-            if ($this->matchesEnvironment($config->getEnvironments())) {
-                $bundleName = $config->getName();
-
-                if (!isset($loadingOrder[$bundleName])) {
-                    $loadingOrder[$bundleName] = [];
-                }
-
-                $bundles[$bundleName] = $config->getClass();
-            }
-        }
-
-        $normalizedOrder = $this->normalizeLoadingOrder($loadingOrder, $replaces);
-        $resolvedOrder   = $this->resolveLoadingOrder($normalizedOrder);
-
-        return $this->order($bundles, $resolvedOrder);
-
-    }
-
-    /**
-     * Creates a configuration collection
-     *
-     * @return ConfigCollection|ConfigInterface[] The configuration collection
-     */
-    protected function createCollection()
-    {
-        $collection = new ConfigCollection();
+        $collection = new ConfigCollection($this->environment);
 
         $this->addBundlesToCollection($collection, $this->findAutoloadFiles(), new JsonParser());
         $this->addBundlesToCollection($collection, $this->findLegacyModules(), new IniParser());
 
-        return $collection;
+        return $collection->all();
     }
 
     /**
@@ -120,7 +68,7 @@ class BundleAutoloader
         return Finder::create()
             ->files()
             ->name('autoload.json')
-            ->in(dirname($this->getRootDir()) . '/vendor')
+            ->in(dirname($this->rootDir) . '/vendor')
         ;
     }
 
@@ -136,7 +84,7 @@ class BundleAutoloader
             ->depth('== 0')
             ->ignoreDotFiles(true)
             ->sortByName()
-            ->in(dirname($this->getRootDir()) . '/system/modules')
+            ->in(dirname($this->rootDir) . '/system/modules')
         ;
     }
 
@@ -159,177 +107,5 @@ class BundleAutoloader
                 $collection->add($factory->create($config));
             }
         }
-    }
-
-    /**
-     * Gets the replaces from the collection
-     *
-     * @param ConfigCollection|ConfigInterface[] $collection The configuration collection
-     *
-     * @return array The replaces array
-     */
-    protected function getReplacesFromCollection(ConfigCollection $collection)
-    {
-        $replace = [];
-
-        foreach ($collection as $bundle) {
-            $name = $bundle->getName();
-
-            foreach ($bundle->getReplace() as $package) {
-                $replace[$package] = $name;
-            }
-        }
-
-        return $replace;
-    }
-
-    /**
-     * Gets the loading order from the collection
-     *
-     * @param ConfigCollection|ConfigInterface[] $collection The configuration collection
-     *
-     * @return array The loading order array
-     */
-    protected function getLoadingOrderFromCollection(ConfigCollection $collection)
-    {
-        // Make sure the core bundle comes first
-        $loadingOrder = [
-            'ContaoCoreBundle' => []
-        ];
-
-        foreach ($collection as $bundle) {
-            $name = $bundle->getName();
-
-            $loadingOrder[$name] = [];
-
-            foreach ($bundle->getLoadAfter() as $package) {
-                $loadingOrder[$name][] = $package;
-            }
-        }
-
-        return $loadingOrder;
-    }
-
-    /**
-     * Checks whether a bundle should be loaded in an environment
-     *
-     * @param array $environments The bundle environments
-     *
-     * @return bool True if the environment matches the bundle environments
-     */
-    protected function matchesEnvironment(array $environments)
-    {
-        return in_array($this->getEnvironment(), $environments) || in_array('all', $environments);
-    }
-
-    /**
-     * Orders the bundles in the resolved loading order
-     *
-     * @param array $bundles The bundles array
-     * @param array $ordered The resolved loading order array
-     *
-     * @return array The ordered bundles array
-     */
-    protected function order(array $bundles, array $ordered)
-    {
-        $return  = [];
-
-        foreach ($ordered as $package) {
-            if (array_key_exists($package, $bundles)) {
-                $return[$package] = $bundles[$package];
-            }
-        }
-
-        return $return;
-    }
-
-    /**
-     * Normalizes the replaces
-     *
-     * @param array $loadingOrder The loading order array
-     * @param array $replace      The replaces array
-     *
-     * @return array The normalized loading order array
-     */
-    protected function normalizeLoadingOrder(array $loadingOrder, array $replace)
-    {
-        foreach ($loadingOrder as $bundleName => &$loadAfter) {
-            if (isset($replace[$bundleName])) {
-                unset($loadingOrder[$bundleName]);
-            } else {
-                $this->replaceBundleNames($loadAfter, $replace);
-            }
-        }
-
-        return $loadingOrder;
-    }
-
-    /**
-     * Replaces the legacy bundle names with their new name
-     *
-     * @param array $loadAfter The load-after array
-     * @param array $replace   The replaces array
-     */
-    protected function replaceBundleNames(array &$loadAfter, array $replace)
-    {
-        foreach ($loadAfter as &$bundleName) {
-            if (isset($replace[$bundleName])) {
-                $bundleName = $replace[$bundleName];
-            }
-        }
-    }
-
-    /**
-     * Tries to resolve the loading order
-     *
-     * @param array $loadingOrder The normalized loading order array
-     *
-     * @return array The resolved loading order array
-     *
-     * @throws UnresolvableLoadingOrderException If the loading order cannot be resolved
-     */
-    protected function resolveLoadingOrder(array $loadingOrder)
-    {
-        $ordered   = [];
-        $available = array_keys($loadingOrder);
-
-        while (!empty($loadingOrder)) {
-            $failed = true;
-
-            foreach ($loadingOrder as $name => $requires) {
-                if (true === $this->canBeResolved($requires, $available, $ordered)) {
-                    $failed    = false;
-                    $ordered[] = $name;
-
-                    unset($loadingOrder[$name]);
-                }
-            }
-
-            if (true === $failed) {
-                throw new UnresolvableLoadingOrderException(
-                    "The bundle loading order could not be resolved.\n" . print_r($loadingOrder, true)
-                );
-            }
-        }
-
-        return $ordered;
-    }
-
-    /**
-     * Checks whether the requirements of a bundle can be resolved
-     *
-     * @param array $requires  The requirements array
-     * @param array $available The installed bundle names
-     * @param array $ordered   The normalized order array
-     *
-     * @return bool True if the requirements can be resolved
-     */
-    protected function canBeResolved(array $requires, array $available, array $ordered)
-    {
-        if (empty($requires)) {
-            return true;
-        }
-
-        return (0 === count(array_diff(array_intersect($requires, $available), $ordered)));
     }
 }
--- src/Autoload/ConfigCollection.php
+++ src/Autoload/ConfigCollection.php
@@ -11,6 +11,8 @@
 
 namespace Contao\Bundle\CoreBundle\Autoload;
 
+use Contao\Bundle\CoreBundle\Exception\UnresolvableLoadingOrderException;
+
 /**
  * Handles a collection of configuration objects
  *
@@ -24,13 +26,37 @@ class ConfigCollection implements \IteratorAggregate
     protected $configs = [];
 
     /**
+     * @var array
+     */
+    protected $ordered;
+
+    /**
+     * @var string
+     */
+    protected $environment;
+
+    /**
+     * Constructor
+     *
+     * @param string $environment The current environment
+     */
+    public function __construct($environment)
+    {
+        $this->environment = $environment;
+    }
+
+    /**
      * Returns an array of configurations
      *
      * @return ConfigInterface[] The configuration array
      */
     public function all()
     {
-        return $this->configs;
+        if (null === $this->ordered) {
+            $this->ordered = $this->getOrderedList();
+        }
+
+        return $this->ordered;
     }
 
     /**
@@ -48,13 +74,200 @@ class ConfigCollection implements \IteratorAggregate
     }
 
     /**
-     * Checks whether the collection is empty
+     * Returns an ordered list of bundles
      *
-     * @return bool True if the collection is empty
+     * @return array The ordered bundles
      */
-    public function isEmpty()
+    protected function getOrderedList()
     {
-        return empty($this->configs);
+        $bundles      = [];
+        $replaces     = $this->getReplaces();
+        $loadingOrder = $this->getLoadingOrder();
+
+        foreach ($this->configs as $config) {
+            if ($this->matchesEnvironment($config->getEnvironments())) {
+                $bundleName = $config->getName();
+
+                if (!isset($loadingOrder[$bundleName])) {
+                    $loadingOrder[$bundleName] = [];
+                }
+
+                $bundles[$bundleName] = $config->getClass();
+            }
+        }
+
+        $normalizedOrder = $this->normalizeLoadingOrder($loadingOrder, $replaces);
+        $resolvedOrder   = $this->resolveLoadingOrder($normalizedOrder);
+
+        return $this->order($bundles, $resolvedOrder);
+    }
+
+    /**
+     * Gets the replaces from the collection
+     *
+     * @return array The replaces array
+     */
+    protected function getReplaces()
+    {
+        $replace = [];
+
+        foreach ($this->configs as $bundle) {
+            $name = $bundle->getName();
+
+            foreach ($bundle->getReplace() as $package) {
+                $replace[$package] = $name;
+            }
+        }
+
+        return $replace;
+    }
+
+    /**
+     * Gets the loading order from the collection
+     *
+     * @return array The loading order array
+     */
+    protected function getLoadingOrder()
+    {
+        // Make sure the core bundle comes first
+        $loadingOrder = [
+            'ContaoCoreBundle' => []
+        ];
+
+        foreach ($this->configs as $bundle) {
+            $name = $bundle->getName();
+
+            $loadingOrder[$name] = [];
+
+            foreach ($bundle->getLoadAfter() as $package) {
+                $loadingOrder[$name][] = $package;
+            }
+        }
+
+        return $loadingOrder;
+    }
+
+    /**
+     * Checks whether a bundle should be loaded in an environment
+     *
+     * @param array $environments The bundle environments
+     *
+     * @return bool True if the environment matches the bundle environments
+     */
+    protected function matchesEnvironment(array $environments)
+    {
+        return in_array($this->environment, $environments) || in_array('all', $environments);
+    }
+
+    /**
+     * Orders the bundles in the resolved loading order
+     *
+     * @param array $bundles The bundles array
+     * @param array $ordered The resolved loading order array
+     *
+     * @return array The ordered bundles array
+     */
+    protected function order(array $bundles, array $ordered)
+    {
+        $return  = [];
+
+        foreach ($ordered as $package) {
+            if (array_key_exists($package, $bundles)) {
+                $return[$package] = $bundles[$package];
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * Normalizes the replaces
+     *
+     * @param array $loadingOrder The loading order array
+     * @param array $replace      The replaces array
+     *
+     * @return array The normalized loading order array
+     */
+    protected function normalizeLoadingOrder(array $loadingOrder, array $replace)
+    {
+        foreach ($loadingOrder as $bundleName => &$loadAfter) {
+            if (isset($replace[$bundleName])) {
+                unset($loadingOrder[$bundleName]);
+            } else {
+                $this->replaceBundleNames($loadAfter, $replace);
+            }
+        }
+
+        return $loadingOrder;
+    }
+
+    /**
+     * Replaces the legacy bundle names with their new name
+     *
+     * @param array $loadAfter The load-after array
+     * @param array $replace   The replaces array
+     */
+    protected function replaceBundleNames(array &$loadAfter, array $replace)
+    {
+        foreach ($loadAfter as &$bundleName) {
+            if (isset($replace[$bundleName])) {
+                $bundleName = $replace[$bundleName];
+            }
+        }
+    }
+
+    /**
+     * Tries to resolve the loading order
+     *
+     * @param array $loadingOrder The normalized loading order array
+     *
+     * @return array The resolved loading order array
+     *
+     * @throws UnresolvableLoadingOrderException If the loading order cannot be resolved
+     */
+    protected function resolveLoadingOrder(array $loadingOrder)
+    {
+        $ordered   = [];
+        $available = array_keys($loadingOrder);
+
+        while (!empty($loadingOrder)) {
+            $failed = true;
+
+            foreach ($loadingOrder as $name => $requires) {
+                if (true === $this->canBeResolved($requires, $available, $ordered)) {
+                    $failed    = false;
+                    $ordered[] = $name;
+
+                    unset($loadingOrder[$name]);
+                }
+            }
+
+            if (true === $failed) {
+                throw new UnresolvableLoadingOrderException(
+                    "The bundle loading order could not be resolved.\n" . print_r($loadingOrder, true)
+                );
+            }
+        }
+
+        return $ordered;
+    }
+
+    /**
+     * Checks whether the requirements of a bundle can be resolved
+     *
+     * @param array $requires  The requirements array
+     * @param array $available The installed bundle names
+     * @param array $ordered   The normalized order array
+     *
+     * @return bool True if the requirements can be resolved
+     */
+    protected function canBeResolved(array $requires, array $available, array $ordered)
+    {
+        if (empty($requires)) {
+            return true;
+        }
+
+        return (0 === count(array_diff(array_intersect($requires, $available), $ordered)));
     }
 
     /**
@@ -62,6 +275,6 @@ class ConfigCollection implements \IteratorAggregate
      */
     public function getIterator()
     {
-        return new \ArrayIterator($this->configs);
+        return new \ArrayIterator($this->all());
     }
 }
