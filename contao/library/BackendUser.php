90f373117a27cb64194912daeed7f2b8adf5d166
Tristan Lins <tristan.lins@bit3.de>
2014-11-02 00:27:43 +0100

    Use router to generate backend menu item urls.

--- contao/library/BackendUser.php
+++ contao/library/BackendUser.php
@@ -526,6 +526,9 @@ class BackendUser extends User
 			Controller::redirect(preg_replace('/(&(amp;)?|\?)mtg=[^& ]*/i', '', Environment::get('request')));
 		}
 
+		/** @var RouterInterface $router */
+		$router = System::getContainer()->get('router');
+
 		$arrInactiveModules = ModuleLoader::getDisabled();
 		$blnCheckInactiveModules = is_array($arrInactiveModules);
 
@@ -563,7 +566,7 @@ class BackendUser extends User
 							$arrModules[$strGroupName]['modules'][$strModuleName]['label'] = (($label = is_array($GLOBALS['TL_LANG']['MOD'][$strModuleName]) ? $GLOBALS['TL_LANG']['MOD'][$strModuleName][0] : $GLOBALS['TL_LANG']['MOD'][$strModuleName]) != false) ? $label : $strModuleName;
 							$arrModules[$strGroupName]['modules'][$strModuleName]['icon'] = !empty($arrModuleConfig['icon']) ? sprintf(' style="background-image:url(\'%s%s\')"', TL_ASSETS_URL, $arrModuleConfig['icon']) : '';
 							$arrModules[$strGroupName]['modules'][$strModuleName]['class'] = 'navigation ' . $strModuleName;
-							$arrModules[$strGroupName]['modules'][$strModuleName]['href'] = TL_SCRIPT . '?do=' . $strModuleName . '&amp;ref=' . TL_REFERER_ID;
+							$arrModules[$strGroupName]['modules'][$strModuleName]['href'] = $router->generate('contao_backend', ['do' => $strModuleName, 'ref' => TL_REFERER_ID]);
 
 							// Mark the active module and its group
 							if (Input::get('do') == $strModuleName)
