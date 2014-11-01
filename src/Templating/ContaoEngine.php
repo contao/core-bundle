128952508fde2f9cf0663f33bb19b96ae9e4185f
Tristan Lins <tristan.lins@bit3.de>
2014-11-01 15:31:34 +0100

    Remove obsolete \Bundle\ from namespace.

--- src/Templating/ContaoEngine.php
+++ src/Templating/ContaoEngine.php
@@ -9,7 +9,7 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
-namespace Contao\Bundle\CoreBundle\Templating;
+namespace Contao\CoreBundle\Templating;
 
 use Contao\BackendTemplate;
 use Contao\Config;
