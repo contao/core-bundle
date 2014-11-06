fc239663fa0c3e874a87a6b6a30833248854edc8
Tristan Lins <tristan.lins@bit3.de>
2014-11-06 22:53:29 +0100

    Support the new loader type.

--- src/Routing/BundlesLoader.php
+++ src/Routing/BundlesLoader.php
@@ -91,6 +91,6 @@ class BundlesLoader extends BaseLoader
      */
     public function supports($resource, $type = null)
     {
-        return 'bundles' === $resource;
+        return 'contao_bundles' === $type;
     }
 }
