f7ce25e248adfbb41f30846d03cc365366acad5f
Leo Feyer <github@contao.org>
2015-04-15 11:44:01 +0200

    Move the event listener to its own namespace.

--- src/EventListener/Hooks/InitializeSystemListener.php
+++ src/EventListener/Hooks/InitializeSystemListener.php
@@ -8,27 +8,24 @@
  * @license LGPL-3.0+
  */
 
-namespace Contao\CoreBundle\EventListener;
+namespace Contao\CoreBundle\EventListener\Hooks;
 
 use Contao\System;
-use Symfony\Component\EventDispatcher\Event;
 
 /**
  * Triggers the "initializeSystem" hook.
  *
  * @author Leo Feyer <https://github.com/leofeyer>
  */
-class InitializeSystemHookListener
+class InitializeSystemListener
 {
     /**
      * Triggers the "initializeSystem" hook.
      *
-     * @param Event $event The event object
-     *
      * @deprecated Deprecated in Contao 4.0, to be removed in Contao 5.0.
      *             Subscribe to the contao.initialize_system event instead.
      */
-    public function onInitializeSystem(Event $event)
+    public function onInitializeSystem()
     {
         if (isset($GLOBALS['TL_HOOKS']['initializeSystem']) && is_array($GLOBALS['TL_HOOKS']['initializeSystem'])) {
             foreach ($GLOBALS['TL_HOOKS']['initializeSystem'] as $callback) {
