4f9ce8463497e2d543976b9bf9b3d8c8ae94b2a1
Leo Feyer <github@contao.org>
2015-05-07 14:09:24 +0200

    Trigger the contao.get_searchable_pages event when rebuilding the search index.

--- src/Resources/contao/classes/RebuildIndex.php
+++ src/Resources/contao/classes/RebuildIndex.php
@@ -10,6 +10,8 @@
 
 namespace Contao;
 
+use Contao\CoreBundle\Event\ContaoCoreEvents;
+use Contao\CoreBundle\Event\ReturnValueEvent;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 
@@ -71,7 +73,14 @@ class RebuildIndex extends \Backend implements \executable
 
 			$arrPages = $this->findSearchablePages();
 
-			// FIXME: trigger an event
+			/** @var KernelInterface $kernel */
+			global $kernel;
+
+			// Dispatch the contao.get_searchable_pages event
+			$event = new ReturnValueEvent($arrPages);
+			$kernel->getContainer()->get('event_dispatcher')->dispatch(ContaoCoreEvents::GET_SEARCHABLE_PAGES, $event);
+
+			$arrPages = $event->getValue();
 
 			// HOOK: take additional pages
 			if (isset($GLOBALS['TL_HOOKS']['getSearchablePages']) && is_array($GLOBALS['TL_HOOKS']['getSearchablePages']))
