d6fbe498627051844cc9537d2a8706994351c1dd
Andreas Schempp <andreas.schempp@terminal42.ch>
2016-04-25 09:45:53 +0200

    Correctly load resources for child classes of ContaoModuleBundle

--- src/DependencyInjection/Compiler/AddResourcesPathsPass.php
+++ src/DependencyInjection/Compiler/AddResourcesPathsPass.php
@@ -41,7 +41,7 @@ class AddResourcesPathsPass implements CompilerPassInterface
         $rootDir = dirname($container->getParameter('kernel.root_dir'));
 
         foreach ($container->getParameter('kernel.bundles') as $name => $class) {
-            if ('Contao\CoreBundle\HttpKernel\Bundle\ContaoModuleBundle' === $class) {
+            if (is_a($class, 'Contao\CoreBundle\HttpKernel\Bundle\ContaoModuleBundle', true)) {
                 $paths[] = sprintf('%s/system/modules/%s', $rootDir, $name);
             } elseif (null !== ($path = $this->getResourcesPathFromClassName($class))) {
                 $paths[] = $path;
