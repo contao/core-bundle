a556133ddfd534552693b07aec2dbfa9963d7d94
Jim Schmid <jim.schmid@1up.io>
2017-01-23 18:52:30 +0100

    Use a different toggleNavigationUrl than the href, as it only works if it lacks the mtg parameter.

--- src/Resources/public/core.js
+++ src/Resources/public/core.js
@@ -28,7 +28,7 @@ var AjaxRequest =
 	 *
 	 * @returns {boolean}
 	 */
-	toggleNavigation: function(el, id) {
+	toggleNavigation: function(el, id, url) {
 		el.blur();
 
 		var item = $(id),
@@ -39,18 +39,18 @@ var AjaxRequest =
 				item.setStyle('display', null);
 				parent.removeClass('node-collapsed').addClass('node-expanded');
 				$(el).store('tip:title', Contao.lang.collapse);
-				new Request.Contao({ url: el.href }).post({'action':'toggleNavigation', 'id':id, 'state':1, 'REQUEST_TOKEN':Contao.request_token});
+				new Request.Contao({ url: url }).post({'action':'toggleNavigation', 'id':id, 'state':1, 'REQUEST_TOKEN':Contao.request_token});
 			} else {
 				item.setStyle('display', 'none');
 				parent.removeClass('node-expanded').addClass('node-collapsed');
 				$(el).store('tip:title', Contao.lang.expand);
-				new Request.Contao({ url: el.href }).post({'action':'toggleNavigation', 'id':id, 'state':0, 'REQUEST_TOKEN':Contao.request_token});
+				new Request.Contao({ url: url }).post({'action':'toggleNavigation', 'id':id, 'state':0, 'REQUEST_TOKEN':Contao.request_token});
 			}
 			return false;
 		}
 
 		new Request.Contao({
-			url: el.href,
+			url: url,
 			evalScripts: true,
 			onRequest: AjaxRequest.displayBox(Contao.lang.loading + ' …'),
 			onSuccess: function(txt) {
