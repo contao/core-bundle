46ef32066064317146421f30049a067d973cd673
Yanick Witschi <yanick.witschi@terminal42.ch>
2017-01-25 18:10:23 +0100

    CS

--- src/Cors/WebsiteRootsConfigProvider.php
+++ src/Cors/WebsiteRootsConfigProvider.php
@@ -38,23 +38,7 @@ class WebsiteRootsConfigProvider implements ProviderInterface
     }
 
     /**
-     * Returns CORS options for $request.
-     *
-     * Any valid CORS option will overwrite those of the previous ones.
-     * The method must at least return an empty array.
-     *
-     * All keys of the bundle's semantical configuration are valid:
-     * - bool allow_credentials
-     * - bool allow_origin
-     * - bool allow_headers
-     * - bool origin_regex
-     * - array allow_methods
-     * - array expose_headers
-     * - int max_age
-     *
-     * @param Request $request
-     *
-     * @return array CORS options
+     * {@inheritdoc}
      */
     public function getOptions(Request $request)
     {
