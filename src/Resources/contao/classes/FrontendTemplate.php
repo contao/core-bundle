192e79108c3e6c1ca32ff3050b4eb0a693504c26
Arne Stappen <meh@agoat.de>
2017-02-17 16:59:34 +0100

    Readd setting default template

--- src/Resources/contao/classes/FrontendTemplate.php
+++ src/Resources/contao/classes/FrontendTemplate.php
@@ -172,6 +172,8 @@ class FrontendTemplate extends \Template
 
 		if ($template === null)
 		{
+			$template = 'block_section';
+			
 			foreach ($this->positions as $position)
 			{
 				if (isset($position[$key]))
