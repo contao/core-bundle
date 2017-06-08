e8f886c8c453362c0996241f6c8ecc9cb6833b7f
Cliff Parnitzky <github@cliff-parnitzky.de>
2017-06-08 23:02:58 +0200

    Surround breacrumb with nav (in file manager) element for better screenreader usage.

--- src/Resources/contao/classes/Backend.php
+++ src/Resources/contao/classes/Backend.php
@@ -1060,9 +1060,11 @@ abstract class Backend extends \Controller
 		// Insert breadcrumb menu
 		$GLOBALS['TL_DCA']['tl_files']['list']['sorting']['breadcrumb'] .= '
 
-<ul id="tl_breadcrumb">
-  <li>' . implode(' &gt; </li><li>', $arrLinks) . '</li>
-</ul>';
+<nav aria-label="' . $GLOBALS['TL_LANG']['MSC']['breadcrumbAriaLabel'] . '" role="navigation">
+  <ul id="tl_breadcrumb">
+    <li>' . implode(' &gt; </li><li>', $arrLinks) . '</li>
+  </ul>
+</nav>';
 	}
 
 
