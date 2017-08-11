c93e5b9ed894e6f221340eb4f7be80fc3fe8ed9c
Arne Stappen <meh@agoat.de>
2017-08-11 14:19:03 +0200

    Clear coding style

--- src/Resources/contao/library/Contao/Combiner.php
+++ src/Resources/contao/library/Contao/Combiner.php
@@ -220,15 +220,12 @@ class Combiner extends \System
 				$strPath = 'assets/' . $strTarget . '/' . str_replace('/', '_', $arrFile['name']) . $this->strMode;
 
 				// Load the existing file
-				if (file_exists(TL_ROOT . '/' . $strPath))
+				if (!file_exists(TL_ROOT . '/' . $strPath))
 				{
-					$return[] = $strUrl . $strPath;
-					continue;
+					$objFile = new \File($strPath);
+					$objFile->write($this->handleScssLess($content, $arrFile));
+					$objFile->close();
 				}
-
-				$objFile = new \File($strPath);
-				$objFile->write($this->handleScssLess($content, $arrFile));
-				$objFile->close();
 
 				$return[] = $strUrl . $strPath;
 			}
