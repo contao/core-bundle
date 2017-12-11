99c1ece28599ebf61b0dc7e86bbdb1c39495c635
Leo Feyer <github@contao.org>
2017-12-11 16:40:38 +0100

    Always check if a password needs to be rehashed.

--- src/Resources/contao/library/Contao/User.php
+++ src/Resources/contao/library/Contao/User.php
@@ -455,18 +455,20 @@ abstract class User extends System implements AdvancedUserInterface, EncoderAwar
 	{
 		$user = new static();
 
+		/** @var Request $request */
+		$request = \System::getContainer()->get('request_stack')->getCurrentRequest();
+		$isLogin = $request->request->has('password') && $request->isMethod(Request::METHOD_POST);
+
 		// Load the user object
 		if ($user->findBy('username', $username) === false)
 		{
-			/** @var Request $request */
-			$request = \System::getContainer()->get('request_stack')->getCurrentRequest();
-			$password = $request->request->get('password');
-
 			// Return if its not a real login attempt
-			if ($password === null && !$request->isMethod(Request::METHOD_POST))
+			if (!$isLogin)
 			{
 				return null;
 			}
+
+			$password = $request->request->get('password');
 
 			/** @var EventDispatcherInterface $eventDispatcher */
 			$eventDispatcher = \System::getContainer()->get('event_dispatcher');
@@ -487,25 +489,28 @@ abstract class User extends System implements AdvancedUserInterface, EncoderAwar
 		}
 
 		// Check if a passwords needs rehashing (see contao/core#8820)
-		$blnNeedsRehash = true;
+		if ($isLogin)
+		{
+			$blnNeedsRehash = true;
 
-		// Handle old sha1() passwords with an optional salt
-		if (preg_match('/^[a-f0-9]{40}(:[a-f0-9]{23})?$/', $user->password))
-		{
-			list($strPassword, $strSalt) = explode(':', $user->password);
-			$blnAuthenticated = ($strPassword === sha1($strSalt . $request->request->get('password')));
-		}
-		else
-		{
-			$blnAuthenticated = password_verify($request->request->get('password'), $user->password);
-			$blnNeedsRehash = password_needs_rehash($user->password, PASSWORD_DEFAULT);
-		}
+			// Handle old sha1() passwords with an optional salt
+			if (preg_match('/^[a-f0-9]{40}(:[a-f0-9]{23})?$/', $user->password))
+			{
+				list($strPassword, $strSalt) = explode(':', $user->password);
+				$blnAuthenticated = ($strPassword === sha1($strSalt . $request->request->get('password')));
+			}
+			else
+			{
+				$blnAuthenticated = password_verify($request->request->get('password'), $user->password);
+				$blnNeedsRehash = password_needs_rehash($user->password, PASSWORD_DEFAULT);
+			}
 
-		// Re-hash the password if the algorithm has changed
-		if ($blnAuthenticated && $blnNeedsRehash)
-		{
-			$user->password = password_hash($request->request->get('password'), PASSWORD_DEFAULT);
-			$user->save();
+			// Re-hash the password if the algorithm has changed
+			if ($blnAuthenticated && $blnNeedsRehash)
+			{
+				$user->password = password_hash($request->request->get('password'), PASSWORD_DEFAULT);
+				$user->save();
+			}
 		}
 
 		$user->setUserFromDb();
