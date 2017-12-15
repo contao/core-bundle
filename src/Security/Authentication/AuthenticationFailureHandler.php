b56c50e7d0402c68bcc03df83053e51d4b83cbf5
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-12-15 17:29:37 +0100

    Use AuthenticationProvider for user locking and access logging

--- src/Security/Authentication/AuthenticationFailureHandler.php
+++ src/Security/Authentication/AuthenticationFailureHandler.php
@@ -13,43 +13,34 @@ declare(strict_types=1);
 namespace Contao\CoreBundle\Security\Authentication;
 
 use Contao\CoreBundle\Routing\ScopeMatcher;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Http\HttpUtils;
-use Symfony\Component\Translation\TranslatorInterface;
 
-class AuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
+class AuthenticationFailureHandler implements AuthenticationFailureHandlerInterface
 {
+    /**
+     * @var HttpUtils
+     */
+    private $httpUtils;
+
     /**
      * @var ScopeMatcher
      */
     private $scopeMatcher;
 
     /**
-     * @var TranslatorInterface
-     */
-    private $translator;
-
-    /**
-     * @param HttpKernelInterface  $httpKernel
      * @param HttpUtils            $httpUtils
      * @param ScopeMatcher         $scopeMatcher
-     * @param TranslatorInterface  $translator
-     * @param array                $options
-     * @param LoggerInterface|null $logger
      */
-    public function __construct(HttpKernelInterface $httpKernel, HttpUtils $httpUtils, ScopeMatcher $scopeMatcher, TranslatorInterface $translator, array $options = [], LoggerInterface $logger = null)
+    public function __construct(HttpUtils $httpUtils, ScopeMatcher $scopeMatcher)
     {
-        parent::__construct($httpKernel, $httpUtils, $options, $logger);
-
+        $this->httpUtils = $httpUtils;
         $this->scopeMatcher = $scopeMatcher;
-        $this->translator = $translator;
     }
 
     /**
@@ -67,16 +58,9 @@ class AuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
         /** @var Session $session */
         $session = $request->getSession();
 
-        if (null === $session) {
-            throw new \RuntimeException('The request did not contain a session object');
+        if (null !== $session) {
+            $session->set(Security::AUTHENTICATION_ERROR, $exception);
         }
-
-        $session->set(Security::AUTHENTICATION_ERROR, $exception);
-
-        $session->getFlashBag()->set(
-            $this->getFlashType($request),
-            $this->translator->trans('ERR.invalidLogin', [], 'contao_default')
-        );
 
         return $this->httpUtils->createRedirectResponse($request, $this->determineTargetUrl($request));
     }
@@ -95,27 +79,5 @@ class AuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
         }
 
         return (string) $request->headers->get('referer', '/');
-    }
-
-    /**
-     * Returns the flash type depending on the provider key.
-     *
-     * @param Request $request
-     *
-     * @return string
-     */
-    private function getFlashType(Request $request): string
-    {
-        $type = '';
-
-        if ($this->scopeMatcher->isFrontendRequest($request)) {
-            $type = 'contao.FE.error';
-        }
-
-        if ($this->scopeMatcher->isBackendRequest($request)) {
-            $type = 'contao.BE.error';
-        }
-
-        return $type;
     }
 }
--- src/Security/Authentication/Provider/ContaoAuthenticationProvider.php
+++ src/Security/Authentication/Provider/ContaoAuthenticationProvider.php
@@ -12,14 +12,19 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Security\Authentication\Provider;
 
+use Contao\Config;
 use Contao\CoreBundle\Framework\ContaoFrameworkInterface;
 use Contao\CoreBundle\Monolog\ContaoContext;
+use Contao\CoreBundle\Security\Exception\LockedException;
+use Contao\FrontendUser;
+use Contao\Idna;
 use Contao\User;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Authentication\Provider\DaoAuthenticationProvider;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -29,9 +34,9 @@ use Symfony\Component\Translation\TranslatorInterface;
 class ContaoAuthenticationProvider extends DaoAuthenticationProvider
 {
     /**
-     * @var Session
+     * @var ContaoFrameworkInterface
      */
-    private $session;
+    private $framework;
 
     /**
      * @var TranslatorInterface
@@ -39,14 +44,14 @@ class ContaoAuthenticationProvider extends DaoAuthenticationProvider
     private $translator;
 
     /**
-     * @var string
+     * @var RequestStack
      */
-    private $providerKey;
+    private $requestStack;
 
     /**
-     * @var ContaoFrameworkInterface
+     * @var \Swift_Mailer
      */
-    private $framework;
+    private $mailer;
 
     /**
      * @var LoggerInterface|null
@@ -59,20 +64,21 @@ class ContaoAuthenticationProvider extends DaoAuthenticationProvider
      * @param string                   $providerKey
      * @param EncoderFactoryInterface  $encoderFactory
      * @param bool                     $hideUserNotFoundExceptions
-     * @param Session                  $session
-     * @param TranslatorInterface      $translator
      * @param ContaoFrameworkInterface $framework
+     * @param TranslatorInterface      $translator
+     * @param RequestStack             $requestStack
+     * @param \Swift_Mailer            $mailer
      * @param LoggerInterface|null     $logger
      */
-    public function __construct(UserProviderInterface $userProvider, UserCheckerInterface $userChecker, $providerKey, EncoderFactoryInterface $encoderFactory, $hideUserNotFoundExceptions, Session $session, TranslatorInterface $translator, ContaoFrameworkInterface $framework, LoggerInterface $logger = null)
+    public function __construct(UserProviderInterface $userProvider, UserCheckerInterface $userChecker, $providerKey, EncoderFactoryInterface $encoderFactory, $hideUserNotFoundExceptions, ContaoFrameworkInterface $framework, TranslatorInterface $translator, RequestStack $requestStack, \Swift_Mailer $mailer, LoggerInterface $logger = null)
     {
         parent::__construct($userProvider, $userChecker, $providerKey, $encoderFactory, $hideUserNotFoundExceptions);
 
-        $this->logger = $logger;
-        $this->session = $session;
-        $this->translator = $translator;
-        $this->providerKey = $providerKey;
         $this->framework = $framework;
+        $this->translator = $translator;
+        $this->requestStack = $requestStack;
+        $this->mailer = $mailer;
+        $this->logger = $logger;
     }
 
     /**
@@ -80,32 +86,153 @@ class ContaoAuthenticationProvider extends DaoAuthenticationProvider
      */
     public function checkAuthentication(UserInterface $user, UsernamePasswordToken $token): void
     {
+        if (!$user instanceof User) {
+            throw new \RuntimeException('Cannot handle non-Contao users.');
+        }
+
         try {
             parent::checkAuthentication($user, $token);
-        } catch (BadCredentialsException $badCredentialsException) {
-            if (!$user instanceof User) {
-                throw $badCredentialsException;
-            }
-
-            if (false === $this->triggerCheckCredentialsHook($user, $token)) {
-                --$user->loginCount;
-                $user->save();
-
-                $this->session->getFlashBag()->set(
-                    $this->getFlashType(),
-                    $this->translator->trans('ERR.invalidLogin', [], 'contao_default')
-                );
-
-                if (null !== $this->logger) {
-                    $this->logger->info(
-                        sprintf('Invalid password submitted for username "%s"', $user->getUsername()),
-                        ['contao' => new ContaoContext(__METHOD__, ContaoContext::ACCESS)]
-                    );
+        } catch (AuthenticationException $exception) {
+            if ($exception instanceof BadCredentialsException) {
+                if ($this->triggerCheckCredentialsHook($user, $token)) {
+                    $this->onSuccess($user);
+                    return;
                 }
 
-                throw $badCredentialsException;
+                $exception = $this->onBadCredentials($user, $exception);
             }
+
+            $this->logAccess($exception->getMessage(), $user);
+
+            throw $exception;
         }
+
+        $this->onSuccess($user);
+    }
+
+    private function onSuccess(User $user)
+    {
+        /** @var Config $config */
+        $config = $this->framework->getAdapter(Config::class);
+
+        $user->lastLogin = $user->currentLogin;
+        $user->currentLogin = time();
+        $user->loginCount = $config->get('loginCount');
+        $user->save();
+
+        $this->logAccess(sprintf('User "%s" has logged in', $user->getUsername()), $user);
+
+        $this->triggerPostLoginHook($user);
+    }
+
+    /**
+     * Count the login attempts and lock the user if it reaches zero.
+     *
+     * @param User                    $user
+     * @param AuthenticationException $exception
+     *
+     * @return AuthenticationException
+     */
+    public function onBadCredentials(User $user, AuthenticationException $exception): AuthenticationException
+    {
+        --$user->loginCount;
+
+        if ($user->loginCount < 1) {
+            /** @var Config $config */
+            $config = $this->framework->getAdapter(Config::class);
+
+            $user->locked = time();
+            $user->loginCount = (int) $config->get('loginCount');
+            $user->save();
+
+            $lockedSeconds = $user->locked + (int) $config->get('lockPeriod') - time();
+            $lockedMinutes = (int) ceil($lockedSeconds / 60);
+
+            $this->sendLockedEmail($user, $lockedMinutes);
+
+            $exception = new LockedException(
+                $lockedSeconds,
+                sprintf('User "%s" has been locked for %s minutes', $user->getUsername(), $lockedMinutes),
+                0,
+                $exception
+            );
+            $exception->setUser($user);
+
+            return $exception;
+        }
+
+        return new BadCredentialsException(
+            sprintf('Invalid password submitted for username "%s"', $user->getUsername()),
+            $exception->getCode(),
+            $exception
+        );
+    }
+
+    /**
+     * Sends an email to the administrator that the account has been locked.
+     *
+     * @param User $user
+     * @param int  $lockedMinutes
+     */
+    private function sendLockedEmail(User $user, int $lockedMinutes): void
+    {
+        $this->framework->initialize();
+
+        /** @var Config $config */
+        $config = $this->framework->getAdapter(Config::class);
+
+        // Send admin notification
+        if ($adminEmail = $config->get('adminEmail')) {
+            $request = $this->requestStack->getMasterRequest();
+
+            if (null === $request) {
+                throw new \RuntimeException('The request stack did not contain a request');
+            }
+
+            $realName = $user->name;
+
+            if ($user instanceof FrontendUser) {
+                $realName = sprintf('%s %s', $user->firstname, $user->lastname);
+            }
+
+            $website = Idna::decode($request->getSchemeAndHttpHost());
+            $subject = $this->translator->trans('MSC.lockedAccount.0', [], 'contao_default');
+
+            $body = $this->translator->trans(
+                'MSC.lockedAccount.1',
+                [$user->getUsername(), $realName, $website, $lockedMinutes],
+                'contao_default'
+            );
+
+            $email = new \Swift_Message();
+
+            $email
+                ->setFrom($adminEmail)
+                ->setTo($adminEmail)
+                ->setSubject($subject)
+                ->setBody($body, 'text/plain')
+            ;
+
+            $this->mailer->send($email);
+        }
+    }
+
+    /**
+     * Logs the access message to the Contao back end.
+     *
+     * @param string $message
+     * @param User   $user
+     */
+    private function logAccess(string $message, User $user)
+    {
+        if (null === $this->logger) {
+            return;
+        }
+
+        $this->logger->info(
+            $message,
+            ['contao' => new ContaoContext(__METHOD__, ContaoContext::ACCESS, $user->getUsername())]
+        );
     }
 
     /**
@@ -138,16 +265,22 @@ class ContaoAuthenticationProvider extends DaoAuthenticationProvider
     }
 
     /**
-     * Returns the flash type depending on the provider key.
+     * Triggers the postLogin hook.
      *
-     * @return string
+     * @param User $user
      */
-    private function getFlashType(): string
+    private function triggerPostLoginHook(User $user): void
     {
-        if ('contao_frontend' === $this->providerKey) {
-            return 'contao.FE.error';
+        $this->framework->initialize();
+
+        if (empty($GLOBALS['TL_HOOKS']['postLogin']) || !\is_array($GLOBALS['TL_HOOKS']['postLogin'])) {
+            return;
         }
 
-        return 'contao.BE.error';
+        @trigger_error('Using the "postLogin" hook has been deprecated and will no longer work in Contao 5.0. Use the security.interactive_login event instead.', E_USER_DEPRECATED);
+
+        foreach ($GLOBALS['TL_HOOKS']['postLogin'] as $callback) {
+            $this->framework->createInstance($callback[0])->{$callback[1]}($user);
+        }
     }
 }
