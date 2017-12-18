86864f42d90f80a9884a45f50d4558153146a45d
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-12-18 18:34:27 +0100

    Restored old code in ModuleLogin and updated for new redirect parameters

--- src/Resources/contao/modules/ModuleLogin.php
+++ src/Resources/contao/modules/ModuleLogin.php
@@ -55,14 +55,9 @@ class ModuleLogin extends \Module
 			return $objTemplate->parse();
 		}
 
-		/** @var Request $request */
-		$request = \System::getContainer()->get('request_stack')->getCurrentRequest();
-
-		// Set the last page visited (see #8632)
-		if ($this->redirectBack && !$request->isMethod(Request::METHOD_POST) && ($strReferer = $this->getReferer()) != \Environment::get('request'))
+		if (!$_POST && $this->redirectBack && ($strReferer = $this->getReferer()) != \Environment::get('request'))
 		{
-			$session = \System::getContainer()->get('session');
-			$session->set('LAST_PAGE_VISITED', $strReferer);
+			$_SESSION['LAST_PAGE_VISITED'] = $strReferer;
 		}
 
 		return parent::generate();
@@ -77,26 +72,36 @@ class ModuleLogin extends \Module
 		/** @var RouterInterface $router */
 		$router = \System::getContainer()->get('router');
 
-		/** @var TokenInterface $token */
-		$token = \System::getContainer()->get('security.token_storage')->getToken();
-
-		// Do not redirect if authentication is successful
-		if ($token !== null && $token->getUser() instanceof FrontendUser && $token->isAuthenticated())
+		if (\System::getContainer()->get('contao.security.token_checker')->hasFrontendUser())
 		{
+			/** @var PageModel $objPage */
+			global $objPage;
+
 			$this->import('FrontendUser', 'User');
+
+			$strRedirect = \Environment::get('base').\Environment::get('request');
+
+			// Redirect to last page visited
+			if ($this->redirectBack && $_SESSION['LAST_PAGE_VISITED'] != '')
+			{
+				$strRedirect = \Environment::get('base').$_SESSION['LAST_PAGE_VISITED'];
+			}
+
+			// Redirect home if the page is protected
+			elseif ($objPage->protected)
+			{
+				$strRedirect = \Environment::get('base');
+			}
 
 			$this->Template->logout = true;
 			$this->Template->formId = 'tl_logout_' . $this->id;
 			$this->Template->slabel = \StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['logout']);
 			$this->Template->loggedInAs = sprintf($GLOBALS['TL_LANG']['MSC']['loggedInAs'], $this->User->username);
-			$this->Template->action = $router->generate('contao_frontend_logout');
-			$this->Template->targetPath = null;
+			$this->Template->action = \System::getContainer()->get('security.logout_url_generator')->getLogoutPath();
+			$this->Template->redirect = $strRedirect;
 
 			if ($this->User->lastLogin > 0)
 			{
-				/** @var PageModel $objPage */
-				global $objPage;
-
 				$this->Template->lastLogin = sprintf($GLOBALS['TL_LANG']['MSC']['lastLogin'][1], \Date::parse($objPage->datimFormat, $this->User->lastLogin));
 			}
 
@@ -116,25 +121,21 @@ class ModuleLogin extends \Module
 			$this->Template->message = $GLOBALS['TL_LANG']['ERR']['invalidLogin'];
 		}
 
-		/** @var Request $request */
-		$request = \System::getContainer()->get('request_stack')->getCurrentRequest();
-
-		$this->Template->targetName = '_target_path';
-		$this->Template->targetPath = $request->getRequestUri();
-
-		/** @var Session $session */
-		$session = \System::getContainer()->get('session');
+		$blnRedirectBack = false;
+		$strRedirect = \Environment::get('base').\Environment::get('request');
 
 		// Redirect to the last page visited
-		if ($this->redirectBack && $session->get('LAST_PAGE_VISITED'))
+		if ($this->redirectBack && $_SESSION['LAST_PAGE_VISITED'] != '')
 		{
-			$this->Template->targetName = '_target_referer';
-			$this->Template->targetPath = $session->get('LAST_PAGE_VISITED');
+			$blnRedirectBack = true;
+			$strRedirect = $_SESSION['LAST_PAGE_VISITED'];
 		}
+
+		// Redirect to the jumpTo page
 		elseif ($this->jumpTo && ($objTarget = $this->objModel->getRelated('jumpTo')) instanceof PageModel)
 		{
 			/** @var PageModel $objTarget */
-			$this->Template->targetPath = $objTarget->getAbsoluteUrl();
+			$strRedirect = $objTarget->getAbsoluteUrl();
 		}
 
 		$this->Template->username = $GLOBALS['TL_LANG']['MSC']['username'];
@@ -143,7 +144,9 @@ class ModuleLogin extends \Module
 		$this->Template->slabel = \StringUtil::specialchars($GLOBALS['TL_LANG']['MSC']['login']);
 		$this->Template->value = \StringUtil::specialchars(\System::getContainer()->get('security.authentication_utils')->getLastUsername());
 		$this->Template->formId = 'tl_login_' . $this->id;
-		$this->Template->autologin = ($this->autologin && \Config::get('autologin') > 0);
+		$this->Template->autologin = $this->autologin;
 		$this->Template->autoLabel = $GLOBALS['TL_LANG']['MSC']['autologin'];
+		$this->Template->redirectBack = $blnRedirectBack;
+		$this->Template->redirect = $strRedirect;
 	}
 }
