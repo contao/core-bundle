de04a446e55282c958236d288749049bd640f42e
M. Vondano <m-vo@users.noreply.github.com>
2018-01-16 23:59:18 +0100

    POC: move tl_theme's callback code into listeners and refactor

---
+++ src/EventListener/DataContainer/Theme.php
@@ -0,0 +1,379 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2018 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\CoreBundle\EventListener\DataContainer;
+
+use Contao\Backend;
+use Contao\BackendUser;
+use Contao\CoreBundle\Exception\AccessDeniedException;
+use Contao\CoreBundle\Image\ImageFactory;
+use Contao\FilesModel;
+use Contao\Image;
+use Contao\StringUtil;
+use Contao\StyleSheets;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class Theme
+{
+    /** @var RequestStack */
+    protected $requestStack;
+
+    /** @var SessionInterface */
+    protected $session;
+
+    /** @var BackendUser */
+    protected $user;
+
+    /** @var ImageFactory */
+    protected $image;
+
+    /**
+     * Theme constructor.
+     *
+     * @param RequestStack          $requestStack
+     * @param SessionInterface      $session
+     * @param TokenStorageInterface $tokenStorage
+     * @param ImageFactory          $image
+     */
+    public function __construct(
+        RequestStack $requestStack,
+        SessionInterface $session,
+        TokenStorageInterface $tokenStorage,
+        ImageFactory $image
+    ) {
+        $this->requestStack = $requestStack;
+        $this->session      = $session;
+        $this->user         = $tokenStorage->getToken()->getUser();
+        $this->image        = $image;
+    }
+
+    /**
+     * Check permissions to edit the table
+     *
+     * @throws AccessDeniedException
+     */
+    public function onCheckPermission(): void
+    {
+        if ($this->user->isAdmin) {
+            return;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        // Check the theme import and export permissions (see #5835)
+        switch ($request->get('key')) {
+            case 'importTheme':
+                if (!$this->user->hasAccess('theme_import', 'themes')) {
+                    throw new AccessDeniedException('Not enough permissions to import themes.');
+                }
+                break;
+
+            case 'exportTheme':
+                if (!$this->user->hasAccess('theme_import', 'themes')) {
+                    throw new AccessDeniedException('Not enough permissions to export themes.');
+                }
+                break;
+        }
+    }
+
+
+    /**
+     * Add an image to each record
+     *
+     * @param array  $row
+     * @param string $label
+     *
+     * @return string
+     */
+    public function onAddPreviewImage(array $row, string $label): string
+    {
+        if (!$row['screenshot']) {
+            return $label;
+        }
+
+        $objFile = FilesModel::findByUuid($row['screenshot']);
+
+        if (null === $objFile) {
+            return $label;
+        }
+
+        $imageUrl = $this->image
+            ->create(TL_ROOT . '/' . $objFile->path, [75, 50, 'center_top'])
+            ->getUrl(TL_ROOT);
+
+        return Image::getHtml($imageUrl, '', 'class="theme_preview"') . ' ' . $label;
+    }
+
+
+    /**
+     * Check for modified style sheets and update them if necessary
+     */
+    public function onUpdateStyleSheet(): void
+    {
+        if ($this->session->get('style_sheet_update_all')) {
+            // todo: make updateStyleSheets() static
+            $styleSheets = new StyleSheets();
+            $styleSheets->updateStyleSheets();
+        }
+
+        $this->session->set('style_sheet_update_all', null);
+    }
+
+
+    /**
+     * Schedule a style sheet update
+     *
+     * This method is triggered when a single theme or multiple themes are
+     * modified (edit/editAll) or duplicated (copy/copyAll).
+     */
+    public function onScheduleUpdate(): void
+    {
+        $this->session->set('style_sheet_update_all', true);
+    }
+
+
+    /**
+     * Return all template folders as array
+     *
+     * @return array
+     */
+    public function onGetTemplateFolders(): array
+    {
+        return $this->doGetTemplateFolders('templates');
+    }
+
+
+    /**
+     * Return all template folders as array
+     *
+     * @param string  $path
+     * @param integer $level
+     *
+     * @return array
+     */
+    // todo: refactor?
+    protected function doGetTemplateFolders(string $path, int $level = 0): array
+    {
+        $return = [];
+
+        foreach (scan(TL_ROOT . '/' . $path) as $file) {
+            if (is_dir(TL_ROOT . '/' . $path . '/' . $file)) {
+                $return[$path . '/' . $file] = str_repeat(' &nbsp; &nbsp; ', $level) . $file;
+                $return                      =
+                    array_merge($return, $this->doGetTemplateFolders($path . '/' . $file, $level + 1));
+            }
+        }
+
+        return $return;
+    }
+
+
+    /**
+     * Return the "import theme" link
+     *
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $class
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function onImportTheme(string $href, string $label, string $title, string $class, string $attributes): string
+    {
+        if (!$this->user->hasAccess('theme_import', 'themes')) {
+            return '';
+        }
+
+        return sprintf(
+            '<a href="%s" class="%s" title="%s"%s>%s</a> ',
+            Backend::addToUrl($href),
+            $class,
+            StringUtil::specialchars($title),
+            $attributes,
+            $label
+        );
+    }
+
+
+    /**
+     * Return the theme store link
+     *
+     * @return string
+     */
+    public function onThemeStore(): string
+    {
+        return sprintf(
+            '<a href="https://themes.contao.org" title="%s" class="header_store" target="_blank" rel="noopener">%s</a>',
+            StringUtil::specialchars($GLOBALS['TL_LANG']['tl_theme']['store'][1]),
+            $GLOBALS['TL_LANG']['tl_theme']['store'][0]
+        );
+    }
+
+
+    /**
+     * Return the "edit CSS" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function onEditCss(
+        array $row,
+        string $href,
+        string $label,
+        string $title,
+        string $icon,
+        string $attributes
+    ): string {
+        return $this->renderButton('css', $row, $href, $label, $title, $icon, $attributes);
+    }
+
+
+    /**
+     * Return the "edit modules" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function onEditModules(
+        array $row,
+        string $href,
+        string $label,
+        string $title,
+        string $icon,
+        string $attributes
+    ): string {
+        return $this->renderButton('modules', $row, $href, $label, $title, $icon, $attributes);
+    }
+
+
+    /**
+     * Return the "edit page layouts" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function onEditLayout(
+        array $row,
+        string $href,
+        string $label,
+        string $title,
+        string $icon,
+        string $attributes
+    ): string {
+        return $this->renderButton('layout', $row, $href, $label, $title, $icon, $attributes);
+    }
+
+
+    /**
+     * Return the "edit image sizes" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function onEditImageSizes(
+        array $row,
+        string $href,
+        string $label,
+        string $title,
+        string $icon,
+        string $attributes
+    ): string {
+        return $this->renderButton('image_sizes', $row, $href, $label, $title, $icon, $attributes);
+    }
+
+
+    /**
+     * Return the "export theme" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function onExportTheme(
+        array $row,
+        string $href,
+        string $label,
+        string $title,
+        string $icon,
+        string $attributes
+    ): string {
+        return $this->renderButton('theme_export', $row, $href, $label, $title, $icon, $attributes);
+    }
+
+
+    /**
+     * Render a button if the user has access to it.
+     *
+     * @param        $field
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    private function renderButton(
+        $field,
+        array $row,
+        string $href,
+        string $label,
+        string $title,
+        string $icon,
+        string $attributes
+    ) {
+        if (!$this->user->hasAccess($field, 'themes')) {
+            return Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)) . ' ';
+        }
+
+        return sprintf(
+            '<a href="%s" title="%s"%s>%s</a> ',
+            Backend::addToUrl($href . '&amp;id=' . $row['id']),
+            StringUtil::specialchars($title),
+            $attributes,
+            Image::getHtml($icon, $label)
+        );
+    }
+}
--- src/Resources/contao/dca/tl_theme.php
+++ src/Resources/contao/dca/tl_theme.php
@@ -30,16 +30,16 @@ $GLOBALS['TL_DCA']['tl_theme'] = array
 		),
 		'onload_callback' => array
 		(
-			array('tl_theme', 'checkPermission'),
-			array('tl_theme', 'updateStyleSheet')
+			array('contao.listener.datacontainer.theme', 'checkPermission'),
+			array('contao.listener.datacontainer.theme', 'updateStyleSheet')
 		),
 		'oncopy_callback' => array
 		(
-			array('tl_theme', 'scheduleUpdate')
+			array('contao.listener.datacontainer.theme', 'scheduleUpdate')
 		),
 		'onsubmit_callback' => array
 		(
-			array('tl_theme', 'scheduleUpdate')
+			array('contao.listener.datacontainer.theme', 'scheduleUpdate')
 		)
 	),
 
@@ -57,7 +57,7 @@ $GLOBALS['TL_DCA']['tl_theme'] = array
 		(
 			'fields'                  => array('name'),
 			'format'                  => '%s',
-			'label_callback'          => array('tl_theme', 'addPreviewImage')
+			'label_callback'          => array('contao.listener.datacontainer.theme', 'addPreviewImage')
 		),
 		'global_operations' => array
 		(
@@ -66,14 +66,14 @@ $GLOBALS['TL_DCA']['tl_theme'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['importTheme'],
 				'href'                => 'key=importTheme',
 				'class'               => 'header_theme_import',
-				'button_callback'     => array('tl_theme', 'importTheme')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'importTheme')
 			),
 			'store' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['store'],
 				'href'                => 'key=themeStore',
 				'class'               => 'header_store',
-				'button_callback'     => array('tl_theme', 'themeStore')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'themeStore')
 			),
 			'all' => array
 			(
@@ -110,35 +110,35 @@ $GLOBALS['TL_DCA']['tl_theme'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['css'],
 				'href'                => 'table=tl_style_sheet',
 				'icon'                => 'css.svg',
-				'button_callback'     => array('tl_theme', 'editCss')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'editCss')
 			),
 			'modules' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['modules'],
 				'href'                => 'table=tl_module',
 				'icon'                => 'modules.svg',
-				'button_callback'     => array('tl_theme', 'editModules')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'editModules')
 			),
 			'layout' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['layout'],
 				'href'                => 'table=tl_layout',
 				'icon'                => 'layout.svg',
-				'button_callback'     => array('tl_theme', 'editLayout')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'editLayout')
 			),
 			'imageSizes' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['imageSizes'],
 				'href'                => 'table=tl_image_size',
 				'icon'                => 'sizes.svg',
-				'button_callback'     => array('tl_theme', 'editImageSizes')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'editImageSizes')
 			),
 			'exportTheme' => array
 			(
 				'label'               => &$GLOBALS['TL_LANG']['tl_theme']['exportTheme'],
 				'href'                => 'key=exportTheme',
 				'icon'                => 'theme_export.svg',
-				'button_callback'     => array('tl_theme', 'exportTheme')
+				'button_callback'     => array('contao.listener.datacontainer.theme', 'exportTheme')
 			)
 		)
 	),
@@ -230,260 +230,293 @@ $GLOBALS['TL_DCA']['tl_theme'] = array
 /**
  * Provide miscellaneous methods that are used by the data configuration array.
  *
- * @author Leo Feyer <https://github.com/leofeyer>
+ * @author     Leo Feyer <https://github.com/leofeyer>
+ *
+ * @deprecated Deprecated since Contao 4.6, to be removed in Contao 5.0
+ *             Use the datacontainer.theme listener instead.
  */
 class tl_theme extends Backend
 {
+    /**
+     * Import the back end user object
+     */
+    public function __construct()
+    {
+        @trigger_error(
+            'Using class tl_theme has been deprecated and will be removed in Contao 5.0. Use the datacontainer.theme listener instead.',
+            E_USER_DEPRECATED
+        );
 
-	/**
-	 * Import the back end user object
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		$this->import('BackendUser', 'User');
-	}
-
-
-	/**
-	 * Check permissions to edit the table
-	 *
-	 * @throws Contao\CoreBundle\Exception\AccessDeniedException
-	 */
-	public function checkPermission()
-	{
-		if ($this->User->isAdmin)
-		{
-			return;
-		}
-
-		// Check the theme import and export permissions (see #5835)
-		switch (Input::get('key'))
-		{
-			case 'importTheme':
-				if (!$this->User->hasAccess('theme_import', 'themes'))
-				{
-					throw new Contao\CoreBundle\Exception\AccessDeniedException('Not enough permissions to import themes.');
-				}
-				break;
-
-			case 'exportTheme':
-				if (!$this->User->hasAccess('theme_import', 'themes'))
-				{
-					throw new Contao\CoreBundle\Exception\AccessDeniedException('Not enough permissions to export themes.');
-				}
-				break;
-		}
-	}
+        parent::__construct();
+        $this->import('BackendUser', 'User');
+    }
 
 
-	/**
-	 * Add an image to each record
-	 *
-	 * @param array  $row
-	 * @param string $label
-	 *
-	 * @return string
-	 */
-	public function addPreviewImage($row, $label)
-	{
-		if ($row['screenshot'] != '')
-		{
-			$objFile = FilesModel::findByUuid($row['screenshot']);
-
-			if ($objFile !== null)
-			{
-				$label = Image::getHtml(\System::getContainer()->get('contao.image.image_factory')->create(TL_ROOT . '/' . $objFile->path, array(75, 50, 'center_top'))->getUrl(TL_ROOT), '', 'class="theme_preview"') . ' ' . $label;
-			}
-		}
-
-		return $label;
-	}
+    /**
+     * Check permissions to edit the table
+     *
+     * @throws Contao\CoreBundle\Exception\AccessDeniedException
+     *
+     * @deprecated
+     */
+    public function checkPermission()
+    {
+        \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onCheckPermission();
+    }
 
 
-	/**
-	 * Check for modified style sheets and update them if necessary
-	 */
-	public function updateStyleSheet()
-	{
-		/** @var Symfony\Component\HttpFoundation\Session\SessionInterface $objSession */
-		$objSession = System::getContainer()->get('session');
-
-		if ($objSession->get('style_sheet_update_all'))
-		{
-			$this->import('StyleSheets');
-			$this->StyleSheets->updateStyleSheets();
-		}
-
-		$objSession->set('style_sheet_update_all', null);
-	}
-
-
-	/**
-	 * Schedule a style sheet update
-	 *
-	 * This method is triggered when a single theme or multiple themes are
-	 * modified (edit/editAll) or duplicated (copy/copyAll).
-	 */
-	public function scheduleUpdate()
-	{
-		/** @var Symfony\Component\HttpFoundation\Session\SessionInterface $objSession */
-		$objSession = System::getContainer()->get('session');
-
-		$objSession->set('style_sheet_update_all', true);
-	}
+    /**
+     * Add an image to each record
+     *
+     * @param array  $row
+     * @param string $label
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function addPreviewImage($row, $label)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onAddPreviewImage(
+            $row,
+            $label
+        );
+    }
 
 
-	/**
-	 * Return all template folders as array
-	 *
-	 * @return array
-	 */
-	public function getTemplateFolders()
-	{
-		return $this->doGetTemplateFolders('templates');
-	}
+    /**
+     * Check for modified style sheets and update them if necessary
+     *
+     * @deprecated
+     */
+    public function updateStyleSheet()
+    {
+        \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onUpdateStyleSheet();
+    }
 
 
-	/**
-	 * Return all template folders as array
-	 *
-	 * @param string  $path
-	 * @param integer $level
-	 *
-	 * @return array
-	 */
-	protected function doGetTemplateFolders($path, $level=0)
-	{
-		$return = array();
-
-		foreach (scan(TL_ROOT . '/' . $path) as $file)
-		{
-			if (is_dir(TL_ROOT . '/' . $path . '/' . $file))
-			{
-				$return[$path . '/' . $file] = str_repeat(' &nbsp; &nbsp; ', $level) . $file;
-				$return = array_merge($return, $this->doGetTemplateFolders($path . '/' . $file, $level+1));
-			}
-		}
-
-		return $return;
-	}
+    /**
+     * Schedule a style sheet update
+     *
+     * This method is triggered when a single theme or multiple themes are
+     * modified (edit/editAll) or duplicated (copy/copyAll).
+     *
+     * @deprecated
+     */
+    public function scheduleUpdate()
+    {
+        \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onScheduleUpdate();
+    }
 
 
-	/**
-	 * Return the "import theme" link
-	 *
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $class
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function importTheme($href, $label, $title, $class, $attributes)
-	{
-		return $this->User->hasAccess('theme_import', 'themes') ? '<a href="'.$this->addToUrl($href).'" class="'.$class.'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.$label.'</a> ' : '';
-	}
+    /**
+     * Return all template folders as array
+     *
+     * @return array
+     *
+     * @deprecated
+     */
+    public function getTemplateFolders()
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onGetTemplateFolders();
+    }
 
 
-	/**
-	 * Return the theme store link
-	 *
-	 * @return string
-	 */
-	public function themeStore()
-	{
-		return '<a href="https://themes.contao.org" title="' . StringUtil::specialchars($GLOBALS['TL_LANG']['tl_theme']['store'][1]) . '" class="header_store" target="_blank" rel="noopener">' . $GLOBALS['TL_LANG']['tl_theme']['store'][0] . '</a>';
-	}
+    /**
+     * Return all template folders as array
+     *
+     * @param string  $path
+     * @param integer $level
+     *
+     * @return array
+     *
+     * @deprecated
+     */
+    protected function doGetTemplateFolders($path, $level = 0)
+    {
+        $return = array();
+
+        foreach (scan(TL_ROOT . '/' . $path) as $file) {
+            if (is_dir(TL_ROOT . '/' . $path . '/' . $file)) {
+                $return[$path . '/' . $file] = str_repeat(' &nbsp; &nbsp; ', $level) . $file;
+                $return                      =
+                    array_merge($return, $this->doGetTemplateFolders($path . '/' . $file, $level + 1));
+            }
+        }
+
+        return $return;
+    }
 
 
-	/**
-	 * Return the "edit CSS" button
-	 *
-	 * @param array  $row
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $icon
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function editCss($row, $href, $label, $title, $icon, $attributes)
-	{
-		return $this->User->hasAccess('css', 'themes') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
-	}
+    /**
+     * Return the "import theme" link
+     *
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $class
+     * @param string $attributes
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function importTheme($href, $label, $title, $class, $attributes)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onImportTheme(
+            $href,
+            $label,
+            $title,
+            $class,
+            $attributes
+        );
+    }
 
 
-	/**
-	 * Return the "edit modules" button
-	 *
-	 * @param array  $row
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $icon
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function editModules($row, $href, $label, $title, $icon, $attributes)
-	{
-		return $this->User->hasAccess('modules', 'themes') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
-	}
+    /**
+     * Return the theme store link
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function themeStore()
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onThemeStore();
+    }
 
 
-	/**
-	 * Return the "edit page layouts" button
-	 *
-	 * @param array  $row
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $icon
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function editLayout($row, $href, $label, $title, $icon, $attributes)
-	{
-		return $this->User->hasAccess('layout', 'themes') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
-	}
+    /**
+     * Return the "edit CSS" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function editCss($row, $href, $label, $title, $icon, $attributes)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onEditCss(
+            $row,
+            $href,
+            $label,
+            $title,
+            $icon,
+            $attributes
+        );
+    }
 
 
-	/**
-	 * Return the "edit image sizes" button
-	 *
-	 * @param array  $row
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $icon
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function editImageSizes($row, $href, $label, $title, $icon, $attributes)
-	{
-		return $this->User->hasAccess('image_sizes', 'themes') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
-	}
+    /**
+     * Return the "edit modules" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function editModules($row, $href, $label, $title, $icon, $attributes)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onEditModules(
+            $row,
+            $href,
+            $label,
+            $title,
+            $icon,
+            $attributes
+        );
+    }
 
 
-	/**
-	 * Return the "export theme" button
-	 *
-	 * @param array  $row
-	 * @param string $href
-	 * @param string $label
-	 * @param string $title
-	 * @param string $icon
-	 * @param string $attributes
-	 *
-	 * @return string
-	 */
-	public function exportTheme($row, $href, $label, $title, $icon, $attributes)
-	{
-		return $this->User->hasAccess('theme_export', 'themes') ? '<a href="'.$this->addToUrl($href.'&amp;id='.$row['id']).'" title="'.StringUtil::specialchars($title).'"'.$attributes.'>'.Image::getHtml($icon, $label).'</a> ' : Image::getHtml(preg_replace('/\.svg$/i', '_.svg', $icon)).' ';
-	}
+    /**
+     * Return the "edit page layouts" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function editLayout($row, $href, $label, $title, $icon, $attributes)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onEditLayout(
+            $row,
+            $href,
+            $label,
+            $title,
+            $icon,
+            $attributes
+        );
+    }
+
+
+    /**
+     * Return the "edit image sizes" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function editImageSizes($row, $href, $label, $title, $icon, $attributes)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onEditImageSizes(
+            $row,
+            $href,
+            $label,
+            $title,
+            $icon,
+            $attributes
+        );
+    }
+
+
+    /**
+     * Return the "export theme" button
+     *
+     * @param array  $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     *
+     * @deprecated
+     */
+    public function exportTheme($row, $href, $label, $title, $icon, $attributes)
+    {
+        return \Contao\System::getContainer()->get('contao.listener.datacontainer.theme')->onExportTheme(
+            $row,
+            $href,
+            $label,
+            $title,
+            $icon,
+            $attributes
+        );
+    }
 }
