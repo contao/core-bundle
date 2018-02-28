9bc77b9d39e08ced2266424f4bd4d87e621984e8
Andreas Schempp <andreas.schempp@terminal42.ch>
2018-02-28 11:26:11 +0700

    Added event listener for {{trans::}} insert tag

---
+++ src/EventListener/InsertTags/TranslationListener.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2018 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Contao\CoreBundle\EventListener\InsertTags;
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+class TranslationListener
+{
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * Replaces the "trans" insert tag.
+     *
+     * @param string $tag
+     *
+     * @return string|false
+     */
+    public function onReplaceInsertTags(string $tag)
+    {
+        $chunks = explode('::', $tag);
+
+        if ('trans' !== $chunks[0]) {
+            return false;
+        }
+
+        $parameters = explode(':', $chunks[2]);
+
+        return $this->translator->trans($chunks[1], $parameters, $chunks[3] ?? null);
+    }
+}
