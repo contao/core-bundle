e151d8379548b1590eed4510dc433af7e2375e8c
Arne Stappen <meh@agoat.de>
2018-03-19 12:22:02 +0100

    Use a global variable for page types to be indexed

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -413,6 +413,12 @@ $GLOBALS['TL_AUTO_ITEM'] = array('items', 'events');
 
 
 /**
+ * Index only the following page types
+ */
+$GLOBALS['TL_INDEX_PAGES'] = array('regular');
+
+
+/**
  * Do not index a page if one of the following parameters is set
  */
 $GLOBALS['TL_NOINDEX_KEYS'] = array('id', 'file', 'token', 'day', 'month', 'year', 'page', 'PHPSESSID');
