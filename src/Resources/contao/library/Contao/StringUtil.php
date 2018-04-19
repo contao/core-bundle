abcd5be96a90313b308d3ec615d947e34ebdc31c
Leo Feyer <github@contao.org>
2018-04-19 08:10:54 +0200

    Simplify the check.

--- src/Resources/contao/library/Contao/StringUtil.php
+++ src/Resources/contao/library/Contao/StringUtil.php
@@ -1049,9 +1049,8 @@ class StringUtil
 			return $blnForceArray ? array() : '';
 		}
 
-		// Return if definitely not a serialized string
-		// See https://github.com/php/php-src/blob/master/ext/standard/var_unserializer.c#L711-L722
-		if (!preg_match('/^[CONRSabdiors]:[0-9]+:/', $varValue))
+		// Not a serialized array (see #1486)
+		if (strncmp($varValue, 'a:', 2) !== 0)
 		{
 			return $blnForceArray ? array($varValue) : $varValue;
 		}
