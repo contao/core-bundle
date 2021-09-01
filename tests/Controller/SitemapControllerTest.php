contao/contao

commit 485bc50898acf8baca7b4a147692ca8407adcfe7

Deprecate the getSearchablePages() method (see #3164)
Description
-----------

fixes https://github.com/contao/contao/pull/3150#issuecomment-874158385

Commits
-------

0e911617 Move getSearchablePages to the front end controller
29418c48 Use parameter instead of config object
0b7b19f8 Rename method that is not related to search
8e359408 The XML Sitemap is not related to indexing protected pages
23cef01c Check content composition if page should be in sitemap
0236420d Call legacy hook for each root page individually
cc2f92df CS
3d6a6a12 Let the response listener handle logged in users
846770c9 Updated unit tests
c7007f6a CS
86f98f5d CS
e1fec264 CS
204662c7 CS

core-bundle/src/Controller/SitemapController.php
@@ -12,12 +12,13 @@ declare(strict_types=1);
 
 namespace Contao\CoreBundle\Controller;
 
-use Contao\Backend;
+use Contao\ArticleModel;
 use Contao\CoreBundle\Event\ContaoCoreEvents;
 use Contao\CoreBundle\Event\SitemapEvent;
+use Contao\CoreBundle\Routing\Page\PageRegistry;
+use Contao\CoreBundle\Security\ContaoCorePermissions;
 use Contao\PageModel;
 use Contao\System;
-use Contao\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,6 +30,16 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class SitemapController extends AbstractController
 {
+    /**
+     * @var PageRegistry
+     */
+    private $pageRegistry;
+
+    public function __construct(PageRegistry $pageRegistry)
+    {
+        $this->pageRegistry = $pageRegistry;
+    }
+
     /**
      * @Route("/sitemap.xml")
      */
@@ -49,25 +60,25 @@ class SitemapController extends AbstractController
             }
         }
 
-        $pages = [];
+        $urls = [];
         $rootPageIds = [];
         $tags = ['contao.sitemap'];
 
         foreach ($rootPages as $rootPage) {
-            /** @var Backend $backend */
-            $backend = $this->get('contao.framework')->getAdapter(Backend::class);
-            $pages = array_merge($pages, $backend->findSearchablePages($rootPage->id, '', true));
-            $pages = $this->callLegacyHook($rootPage, $pages);
+            $pages = $this->getPageAndArticleUrls((int) $rootPage->id);
+            $urls[] = $this->callLegacyHook($rootPage, $pages);
 
             $rootPageIds[] = $rootPage->id;
             $tags[] = 'contao.sitemap.'.$rootPage->id;
         }
 
+        $urls = array_unique(array_merge(...$urls));
+
         $sitemap = new \DOMDocument('1.0', 'UTF-8');
         $sitemap->formatOutput = true;
         $urlSet = $sitemap->createElementNS('https://www.sitemaps.org/schemas/sitemap/0.9', 'urlset');
 
-        foreach ($pages as $url) {
+        foreach ($urls as $url) {
             $loc = $sitemap->createElement('loc', $url);
             $urlEl = $sitemap->createElement('url');
             $urlEl->appendChild($loc);
@@ -80,16 +91,9 @@ class SitemapController extends AbstractController
 
         // Cache the response for a month in the shared cache and tag it for invalidation purposes
         $response = new Response((string) $sitemap->saveXML(), 200, ['Content-Type' => 'application/xml; charset=UTF-8']);
-        $response->setSharedMaxAge(2592000);
+        $response->setSharedMaxAge(2592000); // will be unset by the MakeResponsePrivateListener if a user is logged in
 
         $this->tagResponse($tags);
-
-        // Do not cache the response if a user is logged in.
-        if ($this->getUser() instanceof User) {
-            $response->headers->removeCacheControlDirective('s-maxage');
-            $response->headers->addCacheControlDirective('no-store');
-            $response->setPrivate();
-        }
 
         return $response;
     }
@@ -109,5 +113,51 @@ class SitemapController extends AbstractController
         }
 
         return $pages;
+    }
+
+    private function getPageAndArticleUrls(int $parentPageId): array
+    {
+        /** @var PageModel $pageModelAdapter */
+        $pageModelAdapter = $this->get('contao.framework')->getAdapter(PageModel::class);
+
+        // Since the publication status of a page is not inherited by its child
+        // pages, we have to use findByPid() instead of findPublishedByPid() and
+        // filter out unpublished pages in the foreach loop (see #2217)
+        $pageModels = $pageModelAdapter->findByPid($parentPageId, ['order' => 'sorting']);
+
+        if (null === $pageModels) {
+            return [];
+        }
+
+        /** @var ArticleModel $articleModelAdapter */
+        $articleModelAdapter = $this->get('contao.framework')->getAdapter(ArticleModel::class);
+
+        $result = [];
+
+        // Recursively walk through all subpages
+        foreach ($pageModels as $pageModel) {
+            if ($pageModel->protected && !$this->isGranted(ContaoCorePermissions::MEMBER_IN_GROUPS, $pageModel->groups)) {
+                continue;
+            }
+
+            $isPublished = $pageModel->published && (!$pageModel->start || $pageModel->start <= time()) && (!$pageModel->stop || $pageModel->stop > time());
+
+            if ($isPublished && !$pageModel->requireItem && $this->pageRegistry->supportsContentComposition($pageModel)) {
+                $urls = [$pageModel->getAbsoluteUrl()];
+
+                // Get articles with teaser
+                if (null !== ($articleModels = $articleModelAdapter->findPublishedWithTeaserByPid($pageModel->id, ['ignoreFePreview' => true]))) {
+                    foreach ($articleModels as $articleModel) {
+                        $urls[] = $pageModel->getAbsoluteUrl('/articles/'.($articleModel->alias ?: $articleModel->id));
+                    }
+                }
+
+                $result[] = $urls;
+            }
+
+            $result[] = $this->getPageAndArticleUrls((int) $pageModel->id);
+        }
+
+        return array_merge(...$result);
     }
 }

core-bundle/src/DependencyInjection/Configuration.php
@@ -367,12 +367,12 @@ class Configuration implements ConfigurationInterface
                     ->info('The default search indexer, which indexes pages in the database.')
                     ->addDefaultsIfNotSet()
                     ->children()
-                        ->scalarNode('enable')
+                        ->booleanNode('enable')
                             ->defaultTrue()
                         ->end()
                     ->end()
                 ->end()
-                ->scalarNode('index_protected')
+                ->booleanNode('index_protected')
                     ->info('Enables indexing of protected pages.')
                     ->defaultFalse()
                 ->end()
@@ -380,11 +380,11 @@ class Configuration implements ConfigurationInterface
                     ->info('The search index listener can index valid and delete invalid responses upon every request. You may limit it to one of the features or disable it completely.')
                     ->addDefaultsIfNotSet()
                     ->children()
-                        ->scalarNode('index')
+                        ->booleanNode('index')
                             ->info('Enables indexing successful responses.')
                             ->defaultTrue()
                         ->end()
-                        ->scalarNode('delete')
+                        ->booleanNode('delete')
                             ->info('Enables deleting unsuccessful responses from the index.')
                             ->defaultTrue()
                         ->end()

core-bundle/src/Resources/config/services.yml
@@ -182,6 +182,8 @@ services:
             - controller.service_arguments
 
     Contao\CoreBundle\Controller\SitemapController:
+        arguments:
+            - '@Contao\CoreBundle\Routing\Page\PageRegistry'
         tags:
             - controller.service_arguments
             - container.service_subscriber

core-bundle/src/Resources/contao/classes/Backend.php
@@ -659,9 +659,13 @@ abstract class Backend extends Controller
 	 * @param boolean $blnIsXmlSitemap
 	 *
 	 * @return array
+	 *
+	 * @deprecated Deprecated since Contao 4.12, to be removed in Contao 5.0
 	 */
 	public static function findSearchablePages($pid=0, $domain='', $blnIsXmlSitemap=false)
 	{
+		trigger_deprecation('contao/core-bundle', '4.12', 'Using "Backend::findSearchablePages()" has been deprecated and will no longer work in Contao 5.0.');
+
 		// Since the publication status of a page is not inherited by its child
 		// pages, we have to use findByPid() instead of findPublishedByPid() and
 		// filter out unpublished pages in the foreach loop (see #2217)

contao/core-bundle

commit b07ebdd71e66ce5be1024874d2f39900c4201457

Evaluate robots setting for sitemap (see #3396)
Description
-----------

| Q                | A
| -----------------| ---
| Fixed issues     | Fixes #3395
| Docs PR or issue | -

This bug was introduced in https://github.com/contao/contao/commit/485bc50898acf8baca7b4a147692ca8407adcfe7. Previously the sitemap controller used `Backend::findSearchablePages` which checked the `robots` setting of each page - but it's missing in the current implementation.

Commits
-------

f1936d7e evaulate robots setting for sitemap
89c07323 add test
89628ef2 fix test
3d40340b move condition
216f59b2 Schlauer Fuchs

Co-authored-by: Martin Auswöger <martin@auswoeger.com>

src/Controller/SitemapController.php
@@ -142,7 +142,7 @@ class SitemapController extends AbstractController
 
             $isPublished = $pageModel->published && (!$pageModel->start || $pageModel->start <= time()) && (!$pageModel->stop || $pageModel->stop > time());
 
-            if ($isPublished && !$pageModel->requireItem && $this->pageRegistry->supportsContentComposition($pageModel)) {
+            if ($isPublished && !$pageModel->requireItem && 'noindex,nofollow' !== $pageModel->robots && $this->pageRegistry->supportsContentComposition($pageModel)) {
                 $urls = [$pageModel->getAbsoluteUrl()];
 
                 // Get articles with teaser

tests/Controller/SitemapControllerTest.php
@@ -434,6 +434,56 @@ class SitemapControllerTest extends TestCase
         $this->assertSame($this->getExpectedSitemapContent(['https://www.foobar.com/en/page1.html', 'https://www.foobar.com/en/page1/articles/1.html']), $response->getContent());
     }
 
+    public function testSkipsNoindexNofollowPages(): void
+    {
+        /** @var PageModel&MockObject $page1 */
+        $page1 = $this->mockClassWithProperties(PageModel::class, [
+            'id' => 43,
+            'pid' => 42,
+            'groups' => [],
+            'published' => '1',
+            'robots' => 'index,follow',
+        ]);
+
+        $page1
+            ->expects($this->once())
+            ->method('getAbsoluteUrl')
+            ->willReturn('https://www.foobar.com/en/page1.html')
+        ;
+
+        /** @var PageModel&MockObject $page2 */
+        $page2 = $this->mockClassWithProperties(PageModel::class, [
+            'id' => 44,
+            'pid' => 42,
+            'groups' => [],
+            'published' => '1',
+            'robots' => 'noindex,nofollow',
+        ]);
+
+        $page2
+            ->expects($this->never())
+            ->method('getAbsoluteUrl')
+        ;
+
+        $pages = [
+            42 => [$page1, $page2],
+            43 => null,
+            44 => null,
+            21 => null,
+        ];
+
+        $framework = $this->mockFrameworkWithPages($pages, [43 => null]);
+        $container = $this->mockContainer($framework);
+
+        $controller = new SitemapController($this->mockPageRegistry());
+        $controller->setContainer($container);
+        $response = $controller(Request::create('https://www.foobar.com/sitemap.xml'));
+
+        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertSame('public, s-maxage=2592000', $response->headers->get('Cache-Control'));
+        $this->assertSame($this->getExpectedSitemapContent(['https://www.foobar.com/en/page1.html']), $response->getContent());
+    }
+
     /**
      * @group legacy
      */